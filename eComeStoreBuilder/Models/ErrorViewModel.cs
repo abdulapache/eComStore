08db476e91a8b0fdf8d9dba088b443350f2bef10
Abdul Rahman <rahmanmajeed783@gmail.com>
2023-07-04 06:42:12 +0500

    c# code

---
+++ eComeStoreBuilder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eComeStoreBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
